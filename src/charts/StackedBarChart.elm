72fd2a20c8e8bc66b24f9969f2122b17adc875d7
Alex Emelyanov <holyketzer@gmail.com>
2018-03-08 16:56:18 +0300

    Add hint to mortgage chart

--- src/charts/StackedBarChart.elm
+++ src/charts/StackedBarChart.elm
@@ -10,10 +10,16 @@ import Visualization.Scale as Scale exposing (BandConfig, BandScale, ContinuousS
 import Color exposing (Color)
 import List.Extra as List
 
+import Shared exposing (roundMoney)
+
 
 render : List(String, List Float) -> { width: Int, height: Int } -> Svg msg
 render data windowSize =
-    view (Shape.stack (config data)) { width = toFloat windowSize.width, height = toFloat windowSize.height/2 }
+  let
+    stackedData = Shape.stack (config data)
+    size = { width = toFloat windowSize.width, height = toFloat windowSize.height/2 }
+  in
+    view stackedData size
 
 type alias Year = Int
 
@@ -38,20 +44,36 @@ colors : Int -> List String
 colors size =
   ["#2d9f2c", "#ff7f0e", "#2076b4"]
 
-column : BandScale Year -> (Year, List(Float, Float)) -> Svg msg
-column xScale (year, values) =
+column : List String -> BandScale Year -> (Year, List(Float, Float), List(Float, Float)) -> Svg msg
+column labels xScale (year, values, originalValues) =
   let
-    block color ( upperY, lowerY ) =
-      rect
-        [ x <| toString <| Scale.convert xScale year
-        , y <| toString <| lowerY
-        , width <| toString <| Scale.bandwidth xScale
-        , height <| toString <| (abs <| upperY - lowerY)
-        , fill color
-        ]
-        []
+    xPos = Scale.convert xScale year
+    xHintPos = Basics.min xPos ((Tuple.second <| Scale.range xScale) - maxHintWidth)
+
+    block color (upperY, lowerY) =
+      rect [
+        x <| toString <| xPos,
+        y <| toString <| lowerY,
+        width <| toString <| Scale.bandwidth xScale,
+        height <| toString <| (abs <| upperY - lowerY),
+        fill color
+      ] []
+
+    lineStep = 17
+    maxHintWidth = 150
+    hintTules = List.map3 (,,) labels originalValues (colors <| List.length originalValues)
+    hintValues = List.indexedMap (,) <| List.reverse <| hintTules
+
+    hintLine (index, (label, (from, to), color)) =
+      g [class "hint"] [
+        rect [x <| toString xHintPos, y <| toString <| index * lineStep - 12, width "14", height "14", fill color, stroke "black"] [],
+        text_ [x <| toString (xHintPos + lineStep), y <| toString <| index * lineStep] [text <| label ++ ": " ++ toString (roundMoney <| to - from)]
+      ]
+
+    hint =
+      List.map hintLine hintValues
   in
-    g [ class "column" ] (List.map2 block (colors (List.length values)) values)
+    g [ class "column" ] (hint ++ (List.map2 block (colors (List.length values)) values))
 
 view : StackResult String -> { width: Float, height: Float } -> Svg msg
 view { values, labels, extent } windowSize =
@@ -94,10 +116,16 @@ view { values, labels, extent } windowSize =
       List.map (List.map (\( y1, y2 ) -> ( Scale.convert yScale y1, Scale.convert yScale y2 ))) yearValues
   in
     svg [ width (toString windowSize.width ++ "px"), height (toString windowSize.height ++ "px") ] [
+      Svg.style [] [
+        text """
+          .column .hint { display: none; }
+          .column:hover .hint { display: inline; }
+        """
+      ],
       g [ translate (padding.left - 1) (windowSize.height - padding.bottom) ] [ xAxis ],
       g [ translate (padding.left - 1) padding.top ] [ yAxis ],
       g [ translate padding.left padding.top, class "series" ] <|
-            List.map (column xScale) (List.map2 (,) years scaledValues)
+            List.map (column labels xScale) (List.map3 (,,) years scaledValues yearValues)
     ]
 
 translate : number -> number -> Svg.Attribute msg
